7ce03f24248eb95ce4d05a2bff1ebc40c028bb7f
David Gutierrez <david15.org@gmail.com>
2025-04-26 12:40:33 -0500

    Primer cambio

---
+++ hola.py
@@ -0,0 +1,24 @@
+print("hola mundo")
+
+print(2)
+
+prueba = 2
+prueba = 5
+numero_1 = 'david'
+numero_2 = 'gutierrez'
+
+prueba = (numero_1 * 5) #Aca estamos multiplicando 5 veces numero_1
+texto = "soy programador"
+
+print(type(prueba))
+
+print(prueba)
+print(type(texto))
+print(type(prueba))
+
+'''
+Esto es un comentario de varias lineas
+'''
+
+"""
+"""
