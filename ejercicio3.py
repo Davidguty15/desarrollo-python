ca56838110ba45d8586af169a34dc53d30177ac1
David Gutierrez <david15.org@gmail.com>
2025-05-03 11:26:48 -0500

    Haciendo ejercicios

---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+#Ejercicio 2: Calcula el area de un triángulo. (Crear variables para la base y la altura y el resultado)
+# area = (base * altura) / 2
+base = int(input("Ingrese la base del triángulo: "))
+altura = int(input("Ingrese la altura del triángulo: "))
+area = (base * altura) / 2
+print(f"El área del triángulo es: {area}")67
